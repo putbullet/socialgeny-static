ff576322a22c40b80b580ae335bc84e0b3307f33
putbullet <68425807+putbullet@users.noreply.github.com>
2023-01-29 01:44:02 +0100

    Delete var toggleButton = document.querySelecto.js

--- var toggleButton = document.querySelecto.js
+++
@@ -1,6 +0,0 @@
-var toggleButton = document.querySelector(".sidebar-toggle");
-var sidebarMenu = document.querySelector(".sidebar-menu");
-
-toggleButton.addEventListener("click", function() {
-  sidebarMenu.classList.toggle("open");
-});
